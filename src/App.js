b0d56855ed12ad3f845902e08672d2ba2733f66d
meerasai <meerasaimarella9@gmail.com>
2024-02-28 11:23:50 +0530

    puzzle

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+import BigSquare from "./components/BigSquare";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <BigSquare></BigSquare>
+      </header>
+    </div>
+  );
+}
+
+export default App
